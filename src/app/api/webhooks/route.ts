b314febe85ad0cac3aeeb4e1c50f46603b7287e6
Stepan <97578379+ingerstep@users.noreply.github.com>
2024-06-16 16:19:10 +0700

    Created route for Stripe payment

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,74 @@
+import { db } from "@/db"
+import { stripe } from "@/lib/stripe"
+import { headers } from "next/headers"
+import { NextResponse } from "next/server"
+import Stripe from "stripe"
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.text()
+        const signature = headers().get("stripe-signature")
+
+        if (!signature) {
+            return new Response('Invalid signature', { status: 400 })
+        }
+
+        const event = stripe.webhooks.constructEvent(body, signature, 'need to past env')
+
+        if (event.type === 'checkout.session.completed') {
+            if (!event.data.object.customer_details?.email) {
+                throw new Error('Missing user email')
+            }
+
+            const session = event.data.object as Stripe.Checkout.Session
+
+            const { userId, orderId } = session.metadata || {
+                userId: null,
+                orderId: null
+            }
+
+            if (!userId || !orderId) {
+                throw new Error('Invalid request metadata')
+            }
+
+            const billingAddress = session.customer_details!.address
+            const shippingAddress = session.shipping_details!.address
+
+            await db.order.update({
+                where: {
+                    id: orderId
+                },
+                data: {
+                    isPaid: true,
+                    //lower case ShippinAddress must started
+                    ShippingAddress: {
+                        create: {
+                            name: session.customer_details!.name!,
+                            city: shippingAddress!.city!,
+                            country: shippingAddress!.country!,
+                            postalCode: shippingAddress!.postal_code!,
+                            street: shippingAddress!.line1!,
+                            state: shippingAddress!.state
+                        }
+                    },
+                    billingAddress: {
+                        create: {
+                            name: session.customer_details!.name!,
+                            city: shippingAddress!.city!,
+                            country: shippingAddress!.country!,
+                            postalCode: shippingAddress!.postal_code!,
+                            street: shippingAddress!.line1!,
+                            state: shippingAddress!.state
+                        }
+                    },
+                }
+            })
+        }
+
+        return NextResponse.json({ result: event, ok: true })
+    } catch (err) {
+        console.error(err)
+
+        return NextResponse.json({ message: 'Something went wrong', ok: false }, { status: 500 })
+    }
+}
