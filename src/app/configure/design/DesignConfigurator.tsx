0041498074dc8995397037c2401bd2ac8c502c0f
Stepan <stepan.karelin.1996@mail.ru>
2024-06-17 23:27:11 +0700

    Finished project + build is correct

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -49,7 +49,7 @@ const DesignConfigurator = ({
   const { toast } = useToast();
   const router = useRouter();
 
-  const { mutate: saveConfig } = useMutation({
+  const { mutate: saveConfig, isPending } = useMutation({
     mutationKey: ["save-config"],
     mutationFn: async (args: SaveConfigArgs) => {
       await Promise.all([saveConfiguration(), _saveConfig(args)]);
@@ -397,6 +397,9 @@ const DesignConfigurator = ({
                 )}
               </p>
               <Button
+              isLoading={isPending}
+              disabled={isPending}
+              loadingText="Saving"
                 onClick={() => saveConfig({
                   configId,
                   color: options.color.value,
